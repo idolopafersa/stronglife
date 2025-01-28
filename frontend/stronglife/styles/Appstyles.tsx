f0ed6874abf80fe70b6b38cb31142683508e498d
Pablo fernandez <57355293+idolopafersa@users.noreply.github.com>
2025-01-28 21:13:26 +0100

    Delete frontend/stronglife directory, migrating to expo 52

--- frontend/stronglife/styles/Appstyles.tsx
+++
@@ -1,6 +0,0 @@
-// styles/tabNavigatorStyles.ts
-export const tabNavigatorStyles = {
-    tabBarStyle: { backgroundColor: 'black' },
-    tabBarLabelStyle: { color: '#A5D32D' },
-    tabBarIconColor: '#A5D32D',
-  };
