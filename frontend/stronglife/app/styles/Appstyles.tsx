096e2e3c5eead7a62066758f8e6bf403299ac77a
idolopafersa <pafersa2003@gmail.com>
2025-01-28 21:14:08 +0100

    Migrated to expo 52, fixed routing, Login and select Date

--- frontend/stronglife/app/styles/Appstyles.tsx
+++
@@ -1,6 +0,0 @@
-// app/styles/Appstyles.tsx
-export const tabNavigatorStyles = {
-  tabBarStyle: { backgroundColor: 'black' },
-  tabBarLabelStyle: { color: '#A5D32D' },
-  tabBarIconColor: '#A5D32D',
-};
